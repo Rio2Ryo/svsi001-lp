139733315278a6b2989fda508493ebb4aa329890
go2020h <go2020.hidane.inc@gmail.com>
2025-07-31 14:03:45 +0900

    constants.jsのID変更

--- constants/constants.js
+++ constants/constants.js
@@ -1,2 +1,2 @@
 // CLIENT_ID constant which is used for OAuth authentication
-export const CLIENT_ID = "cdb13f50-0efd-4998-a3ee-f8d5585b65e3";
+export const CLIENT_ID = "03253fad-228c-4df4-9272-0bf9a5421fb5";
