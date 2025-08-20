75ef060afcb81ffd37b113082ccdf458e4fabf89
go2020h <go2020.hidane.inc@gmail.com>
2025-08-20 13:54:39 +0900

    fix

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,5 +1,7 @@
 "use client";
 import { useEffect, useState } from "react";
+import Image from 'next/image'
+
 
 export default function HeroSection() {
   const [isVisible, setIsVisible] = useState(false);
