0abf17b890f6d7579e1823a78e143da981ca8522
go2020h <go2020.hidane.inc@gmail.com>
2025-08-30 11:46:05 +0900

    fix

--- next.config.js
+++ next.config.js
@@ -1,14 +1,5 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    reactStrictMode: false,
-  
-    // /en/__ecom/checkout を内部で /__ecom/checkout に差し替える
-    async rewrites() {
-      return [
-        { source: '/en/__ecom/checkout', destination: '/__ecom/checkout' },
-      ];
-    },
-  };
-  
-  module.exports = nextConfig;
-  
+/** @type {import('next').NextConfig} */ 
+const nextConfig = 
+{ reactStrictMode: false, };
+
+ module.exports = nextConfig;
